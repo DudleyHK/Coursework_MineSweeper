53ab8b3f76df6f7c45bee40dfa1c66ac566c7c67
DudleyHK <dudleydawes@hotmail.co.uk>
2016-01-20 11:12:23 +0000

    New Error Handling

---
+++ MineSweeper/ErrorHandling.cpp
@@ -0,0 +1,26 @@
+/*
+
+
+
+
+*/
+
+#include "ErrorHandling.h"
+
+#include <iostream>
+using namespace std;
+using namespace ErrorHandling;
+
+
+
+
+/*This function takes the users input as a string allowing the program to 
+check the whole input for invalid characters. The function will return an 
+error message*/
+string ErrorHandling::checkHeightWidth(string input)
+{
+
+
+
+
+}
